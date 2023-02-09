94480f0a09b213db6a2ff39556725b823d4bc2e4
PankajKTech <pankajkatech@gmail.com>
2023-02-09 23:51:14 +0530

    Updated

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+
+function onToggleMenu(icon) {
+    icon.name = icon.name === 'menu-outline' ? 'close-outline' : 'menu-outline';
+    const nav = document.querySelector('ul');
+    nav.classList.toggle('top-[5%]');
+}
