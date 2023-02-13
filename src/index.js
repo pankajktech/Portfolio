ecac56981d5cf8d904ac14ffe81b259a5a869c5d
PankajKTech <pankajkatech@gmail.com>
2023-02-13 22:27:35 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 // Hamburger Menu
 function onToggleMenu(icon) {
-    icon.name = icon.name === 'menu-outline' ? 'close-outline' : 'menu-outline';
+    icon.name ='menu-outline' ? 'close-outline' : 'menu-outline';
     const nav = document.querySelector('ul');
     nav.classList.toggle('top-[6%]');
 }
