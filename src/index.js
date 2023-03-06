67580302cfaca597ee523ad2e94190c506069f3e
PankajKTech <pankajkatech@gmail.com>
2023-03-06 10:03:33 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Typed from "typed.js";
+
 // Hamburger Menu
 function onToggleMenu(icon) {
   icon.name = icon.name === "menu-outline" ? "close-outline" : "menu-outline";
@@ -6,15 +6,5 @@ function onToggleMenu(icon) {
   nav.classList.toggle("top-[6%]");
 }
 
-const Typed = document.querySelector(".typing");
-
-// const typed = new Typed(".typing", {
-//   strings: ["Web Developer", "Web Designer", "Freelancer"],
-//   typeSpeed: 100,
-//   backSpeed: 100,
-//   backDelay: 1000,
-//   loop: true,
-//   loopCount: Infinity,
-// });
 
 
